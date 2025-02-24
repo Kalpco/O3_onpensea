1c3ab22a37942ec34881d291d6dbc1a4f37f7879
Aditya Harendra Mall <addasjii@gmail.com>
2025-02-24 11:10:19 +0530

    commented external storag1e : aditya

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,8 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await dotenv.load(fileName: "lib/.env.uat");
-
+  await dotenv.load(fileName: "lib/.env.production");
   Get.put(WishlistController());
-
   runApp(const App());
 }
 
